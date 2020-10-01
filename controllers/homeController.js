bb07bf1a33f5284d6a162dab1ca8613f968a134f
Guinn <Dan.Guinn@fike.com>
2020-10-01 12:48:44 -0500

    Modified home controller and template to display stats.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,42 +1,58 @@
 
 const router = require('../router');
+
+//Get Game Model:
 const gameModel = require('../models/gameModel');
 const gameInfo=gameModel.gameInfo;
 const guestures=gameModel.gameGestures;
 
+const statsModel = require('../models/statsModel');
+const statsMod = new statsModel();
+
 exports.processReq=(req,res)=>{
-  //Some important variables at your disposal:
   let page=req.path;
   let params=req.query;
+
   let routeObj=router.getCurRouteObj("Home");
   let template=routeObj.template;
-  //console.log(routeObj);
 
-  //OPTIONS FOR RETURN:
-  //1) Normal Express Send()
-  //res.send(`This is the ${page} page. You sent the following parameters: ${JSON. stringify(params)}`);
+  const asyncResults = async () => {
+    //Get Game Stats:
+    stats={};
+    let win = await statsMod.readPlayerWin();
+    stats.win=win[0].win;
+    let lose = await statsMod.readPlayerLose();
+    stats.lose=lose[0].lose;
+    let draw = await statsMod.readPlayerDraw();
+    stats.draw=draw[0].draw;
+    let player1Gestures=await statsMod.readPlayerGestures({
+      player:'Player'
+    });
+    stats.user_gestures=player1Gestures;
 
-  //2) Express Send File: You may want to include path
-  //var path = require('path');
-  //res.sendFile(path.join(__dirname + '../../views/'+template));
+    let player2Gestures=await statsMod.readPlayerGestures({
+      player:'Computer'
+    });
+    stats.computer_gestures=player2Gestures;
 
-  //3) Data Model call here = Get Model for Home:
-  //console.log(appModel.getAppContent("Home"));
-  var homeBodyContent="undefined";
+    console.log(stats);
+
+    res.render(template, {locals:
+      {
+        title: gameInfo.name,
+        code:gameInfo.code,
+        gameExplanation:gameInfo.explanation,
+        challengeMsg:gameInfo.challengeMsg,
+        exampleInfo:gameInfo.exampleInfo,
+        guestures:guestures,
+        gameStats:stats
+
+      }
+    });
+
+  };
+  asyncResults();
 
 
-  //console.log(gameInfo);
-  //console.log(guestures);
 
-  //4) es6Renderer
-  res.render(template, {locals:
-    {
-      title: gameInfo.name,
-      code:gameInfo.code,
-      gameExplanation:gameInfo.explanation,
-      challengeMsg:gameInfo.challengeMsg,
-      exampleInfo:gameInfo.exampleInfo,
-      guestures:guestures
-    }
-  });
 }
